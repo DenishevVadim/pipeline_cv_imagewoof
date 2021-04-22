ac922a57778a40d0212bd2270cdc28a35bb23d29
vadim <Vadimka36@yandex.ru>
2021-04-22 18:09:36 +0300

    add transform functions

--- NN_tools/dataset.py
+++ NN_tools/dataset.py
@@ -19,6 +19,40 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
 
+def get_train_transforms():
+    return A.Compose(
+        [
+            A.RandomSizedCrop((140,140), width=160, height=160, p=0.25),
+            A.OneOf([
+                A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2,
+                                     val_shift_limit=0.2, p=0.9),
+                A.RandomBrightnessContrast(brightness_limit=0.2,
+                                           contrast_limit=0.2, p=0.9),
+            ],p=0.9),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Rotate(limit=45, p=1),
+            A.ChannelShuffle(p=0.05),
+            A.FancyPCA(),
+            A.GaussNoise(p=0.25),
+            A.Blur(blur_limit=4,p=0.1),
+            A.Cutout(num_holes=8, max_h_size=4, max_w_size=4, fill_value=0, p=0.1),
+            A.Resize (160, 160,),
+            A.Normalize (mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(p=1.0),
+        ],
+        p=1.0
+    )
+def get_val_transforms():
+    return A.Compose(
+        [
+            A.Resize (160, 160,),
+            A.Normalize (mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(p=1.0),
+        ],
+        p=1.0
+    )
+
 class CustomDataset(Dataset):
     def __init__(self, folder, transform=None):
         self.folder = folder
